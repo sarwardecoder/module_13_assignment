638e261c7a63003a459822e70c0039f7f2a14918
Sarwar Jahan <sarwar3@gmail.com>
2024-12-11 01:40:57 +0600

    laravel assignment initial commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Request;
+
+
+class ProfileController extends Controller
+{
+
+    public function index($id)
+    {
+
+
+        $name = "Donald Trump";
+        $age = "75";
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+        $status_code = 200;
+        $access_token = "123-XYZ";
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+        $response = response($data, $status_code);
+        $response->cookie('sarwars_cookie', $access_token, $minutes, $path, $domain, $secure, $httpOnly);
+        return $response;
+
+    }
+
+}
